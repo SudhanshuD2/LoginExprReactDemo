de5e6d993bfc2fe115815aaa213f2ba53e0418b8
Samaksh Bansal <samaksh199@gmail.com>
2026-05-30 23:33:40 +0530

    Delete backend directory

--- backend/pools/pool.js
+++
@@ -1,9 +0,0 @@
-const mysql2 = require('mysql2/promise')
-const pool = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'logistic_db'
-})
-
-module.exports = pool
